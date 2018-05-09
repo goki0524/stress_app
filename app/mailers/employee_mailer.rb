a1217cbaa98edda6db7348f5f99e2531a48e0f9a
goki <goki0524@gmail.com>
2018-05-09 11:54:44 +0000

    Fix send qrcode View

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -2,11 +2,8 @@ class EmployeeMailer < ApplicationMailer
   
   def send_interview_mail(employee, companies)
     @employee = employee
-    
     company_emails = companies.map{ |c| c.company_email }
-    
     for company_email in company_emails
-      #TODO: メソッドの引数にブロック変数は使えない
       mail to: company_email, subject: "高ストレス者からの面談希望" 
     end
   end
