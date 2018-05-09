a3aee540dcd68d72527cf11e22032733e249de10
goki <goki0524@gmail.com>
2018-05-09 09:43:27 +0000

    Fix send_interview_mail

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -2,8 +2,12 @@ class EmployeeMailer < ApplicationMailer
   
   def send_interview_mail(employee, companies)
     @employee = employee
-    companies.each do |c|
-      mail to: c.company_email, subject: "高ストレス者からの面談希望" 
+    
+    company_emails = companies.map{ |c| c.company_email }
+    
+    for company_email in company_emails
+      #TODO: メソッドの引数にブロック変数は使えない
+      mail to: company_email, subject: "高ストレス者からの面談希望" 
     end
   end
   
