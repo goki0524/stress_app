6d2233664cbca5a32555aebc6dd5a1c3dc578d5c
goki <goki0524@gmail.com>
2018-05-10 02:49:47 +0000

    Fix Prevent dual login

--- test/mailers/previews/employee_mailer_preview.rb
+++ test/mailers/previews/employee_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/employee_mailer
 class EmployeeMailerPreview < ActionMailer::Preview
+  def send_interview_mail
+    employee = Employee.first
+    companies = employee.departments.map{ |d| d.company }
+    EmployeeMailer.send_interview_mail(employee, companies)
+  end
 
 end
