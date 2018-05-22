2e08aaaf06211d1ff0e915b8fa145dd54963d41d
goki <goki0524@gmail.com>
2018-05-22 01:08:53 +0000

    fix

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,5 +1,7 @@
 class InquiryMailer < ActionMailer::Base
   
+  default from: "goki0524@gmail.com"
+  
   def received_email(inquiry)
     @inquiry = inquiry
     mail to: "goki0524@gmail.com", subject: "Stress Appよりお問い合わせの通知"
