756d457991fe609cceba31aec78d6f29e31be13f
goki <goki0524@gmail.com>
2018-05-26 01:41:59 +0000

    Add encoding utf-8

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -4,8 +4,13 @@ class EmployeeMailer < ApplicationMailer
   #ストレス値を提出した実施回の会社と所属した部署の会社が同じかどうか
   def send_interview_mail(employee, companies)
     @employee = employee
+    @companies = companies
+    @company = nil
     for company in companies
+      @company = company
+      mail.body.decoded.to_s.force_encoding('utf-8')
       mail to: company.company_email, subject: "高ストレス者からの面談希望の通知" 
+      @company = nil
     end
   end
   
