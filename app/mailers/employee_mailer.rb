887113bc1387a4e360836cab755813596ef603ff
goki <goki0524@gmail.com>
2018-05-26 00:55:44 +0000

    Fix send interview mail

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -1,16 +1,11 @@
 class EmployeeMailer < ApplicationMailer
-  
+  require "base64"
   #TODO: テスト必要.面談希望時のメイラーが正しく動作しているか
   #ストレス値を提出した実施回の会社と所属した部署の会社が同じかどうか
   def send_interview_mail(employee, companies)
     @employee = employee
-    @companies = companies
-    @company = nil
     for company in companies
-      @company = company
-      mail.body.decoded.encode("UTF-8", mail.charset)
       mail to: company.company_email, subject: "高ストレス者からの面談希望の通知" 
-      @company = nil
     end
   end
   
